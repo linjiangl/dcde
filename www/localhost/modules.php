31af4dcde645ab04796f5960668794135d751c1f
jianglin <8257796@qq.com>
2022-04-24 18:07:48 +0800

    项目升级优化

--- www/localhost/modules.php
+++ www/localhost/modules.php
@@ -1,11 +1,21 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+echo '<h1 style="text-align: center;">欢迎使用DNMP！</h1>';
+echo '<h2>版本信息</h2>';
+
+echo '<ul>';
 echo '<li>PHP版本：', PHP_VERSION, '</li>';
 echo '<li>Nginx版本：', $_SERVER['SERVER_SOFTWARE'], '</li>';
 echo '<li>MySQL服务器版本：', getMysqlVersion(), '</li>';
 echo '<li>Redis服务器版本：', getRedisVersion(), '</li>';
+echo '<li>MongoDB服务器版本：', getMongoVersion(), '</li>';
 echo '</ul>';
+
 echo '<h2>已安装扩展</h2>';
 printExtensions();
+
 /**
  * 获取MySQL版本
  */
@@ -13,7 +23,7 @@ function getMysqlVersion()
 {
     if (extension_loaded('PDO_MYSQL')) {
         try {
-            $dbh = new PDO('mysql:host=mysql;dbname=mysql', 'root', '123456');
+            $dbh = new PDO('mysql:host=mysql8;dbname=mysql', 'root', '123456');
             $sth = $dbh->query('SELECT VERSION() as version');
             $info = $sth->fetch();
         } catch (PDOException $e) {
@@ -23,7 +33,9 @@ function getMysqlVersion()
     } else {
         return 'PDO_MYSQL 扩展未安装 ×';
     }
+
 }
+
 /**
  * 获取Redis版本
  */
@@ -42,6 +54,28 @@ function getRedisVersion()
         return 'Redis 扩展未安装 ×';
     }
 }
+
+/**
+ * 获取MongoDB版本
+ */
+function getMongoVersion()
+{
+    if (extension_loaded('mongodb')) {
+        try {
+            $manager = new MongoDB\Driver\Manager('mongodb://root:123456@mongodb:27017');
+            $command = new MongoDB\Driver\Command(array('serverStatus'=>true));
+
+            $cursor = $manager->executeCommand('admin', $command);
+
+            return $cursor->toArray()[0]->version;
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    } else {
+        return 'MongoDB 扩展未安装 ×';
+    }
+}
+
 /**
  * 获取已安装扩展列表
  */
@@ -49,7 +83,8 @@ function printExtensions()
 {
     echo '<ol>';
     foreach (get_loaded_extensions() as $i => $name) {
-        echo "<li>", $name, '</li>';
+        echo "<li>", $name, '=', phpversion($name), '</li>';
     }
     echo '</ol>';
 }
+
