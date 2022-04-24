ca3e24f6113bdc0422dbc67605f67cf23d3eee18
jianglin <8257796@qq.com>
2022-04-24 19:09:20 +0800

    phpmyadmin 配置修改

--- services/phpmyadmin/config.user.inc.php
+++ services/phpmyadmin/config.user.inc.php
@@ -1,19 +1,19 @@
 <?php
 /**
+ * mysql8,mysql5 example
  * @example https://docs.phpmyadmin.net/en/latest/config.html
  */
-/*mysql,mysql5 example
+
 $cfg['blowfish_secret']='multiServerExample70518';
 
 $i = 0;
 
 $i++; // server 1 :
 $cfg['Servers'][$i]['verbose']   = 'no1';
-$cfg['Servers'][$i]['host']      = 'mysql';
-
-
-$i++; // server 2 :
-$cfg['Servers'][$i]['verbose']   = 'no2';
-$cfg['Servers'][$i]['host']      = 'mysql5';
-*/
+$cfg['Servers'][$i]['host']      = 'mysql8';
+//
+//
+//$i++; // server 2 :
+//$cfg['Servers'][$i]['verbose']   = 'no2';
+//$cfg['Servers'][$i]['host']      = 'mysql5';
 
